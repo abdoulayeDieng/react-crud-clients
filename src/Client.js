07da56651fd30f51d7f33a9d088b45ecfdbe4d9c
Abdoulaye Dieng <adieng@gmail.com>
2019-10-26 10:53:35 +0000

    Composant fonctionnel Client

--- src/Client.js
+++ src/Client.js
@@ -1,16 +1,12 @@
 import React from "react";
 
-class Client extends React.Component {
-  render() {
-    return (
-      <li key={this.props.details.id}>
-        {this.props.details.nom}
-        <button onClick={() => this.props.onDelete(this.props.details.id)}>
-          X
-        </button>
-      </li>
-    );
-  }
+function Client(props) {
+  return (
+    <li key={props.details.id}>
+      {props.details.nom}
+      <button onClick={() => props.onDelete(props.details.id)}>X</button>
+    </li>
+  );
 }
 
 export default Client;
